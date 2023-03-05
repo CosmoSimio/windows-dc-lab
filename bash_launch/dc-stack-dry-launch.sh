8c91a0273f15ac4c41b729170fc5e6a1b83a20b2
Xolo-itzcuintle <78327139+Xolo-itzcuintle@users.noreply.github.com>
2023-03-05 13:42:53 -0600

    adding sub folders

---
+++ bash_launch/dc-stack-dry-launch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set the AWS region
+region="us-east-2"
+
+# Set the CloudFormation stack name
+stackName="isaac-windows-dc-labstack"
+
+# Set the path to the CloudFormation template
+templatePath="./windows-dc-labstack.yaml"
+
+# Set the parameter overrides for the CloudFormation stack
+# Set the parameter overrides for the CloudFormation stack
+params="VpcId=vpc-12345678"
+params="$params SecurityGroupId=sg-12345678"
+params="$params KeyName=mykeypair"
+params="$params DomainControllerName=dcinstance"
+params="$params CollectorServerName=collectorinstance"
+params="$params DomainName=mydomain.local"
+params="$params DomainAdmin1=admin1"
+params="$params DomainAdmin1Password=Password123"
+params="$params DomainAdmin2=admin2"
+params="$params DomainAdmin2Password=Password456"
+params="$params DomainAdmin3=admin3"
+params="$params DomainAdmin3Password=Password789"
+params="$params MSSQLInstanceName=sqlinstance"
+params="$params MSSQLPassword=MyPassw0rd"
+
+# Deploy the stack
+aws cloudformation deploy \
+    --stack-name $stackName \
+    --template-file $templatePath \
+    --parameter-overrides $params \
+    --no-execute-changeset \
+    --region $region
